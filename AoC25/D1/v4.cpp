fee96d2df968016b4ae85e03c29e53c601787cd4
Neil <neilpraj@gmail.com>
2026-01-11 07:33:29 -0500

    added debug statements and conditional handling

---
+++ AoC25/D1/v4.cpp
@@ -0,0 +1,59 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+// #define DEBUG
+
+void debugPrint(const std::string &message) {
+#ifdef DEBUG
+  std::cout << message << std::endl;
+#endif
+}
+
+const int POSITIONS = 100;
+const int START_POS = 50;
+
+int partOne() {
+  std::string s;
+  std::ifstream rfile;
+
+  int rot = START_POS;
+  int pass = 0;
+
+  rfile.open("./code.txt");
+  // rfile.open("./test.txt");
+
+  if (rfile.is_open()) {
+    while (std::getline(rfile, s)) {
+      if (s.empty())
+        return 0;
+
+      debugPrint(s);
+      int dis = std::stoi(s.substr(1));
+
+      if (s[0] == 'R') {
+        rot = rot + dis;
+      } else {
+        rot = rot - dis;
+      }
+
+      debugPrint("Raw rotation: " + std::to_string(rot));
+
+      rot = ((rot % POSITIONS) + POSITIONS) % POSITIONS;
+
+      debugPrint("Scaled rotation: " + std::to_string(rot));
+
+      if (rot == 0)
+        pass++;
+    }
+    rfile.close();
+    return pass;
+  }
+  return 0;
+}
+
+int main() {
+  int pass;
+  pass = partOne();
+  std::cout << pass << std::endl;
+}
